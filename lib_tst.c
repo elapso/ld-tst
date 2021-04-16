8d190f1b0672290f7f26e94c476d94d57d8805be
elapso <elapso@1.1>
2021-04-16 18:42:25 +0300

    [-] absolute faunction

--- lib_tst.c
+++ lib_tst.c
@@ -1,27 +1,5 @@
 #include "lib_tst.h"
 
-void foo(char* f)
-{
-    struct timespec time[2];
-    time[0].tv_nsec = UTIME_NOW;
-    time[1].tv_nsec = UTIME_NOW;
- 
-    int fd;
-    fd = open(f,O_RDONLY);
- 
-    if(fd == -1){
-        printf("open file failed. %s\n",  strerror(errno));
-    }
-    else{
-        if(-1 == futimens(fd,time)){
-            printf("modify time failed.\n");
-        }
-        else{
-            printf("modify time succeed.\n");
-        }
-    }
-  
-}
 
 
 void tst_getenv(const char* n)
